7ba1ea3ec65120d93d194790ab3f313217490744
Christoph Barbian <christoph.barbian@sap.com>
2023-06-21 21:38:39 +0200

    fix order of kubebuilder markers and regular comments (to get clean godocs)

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -3,9 +3,10 @@ SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and redis-operat
 SPDX-License-Identifier: Apache-2.0
 */
 
-// Package v1alpha1 contains API Schema definitions for the cache v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=cache.cs.sap.com
+
+// Package v1alpha1 contains API Schema definitions for the cache v1alpha1 API group
 package v1alpha1
 
 import (
